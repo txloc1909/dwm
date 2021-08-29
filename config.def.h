6a16ab28e81c2300483b3c7140bc43b69abc5424
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2021-08-29 17:01:50 +0700

    patch successfully focusonclick

--- config.def.h
+++ config.def.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static const char *fonts[]          = { "Ubuntu Mono:size=9", "monospace:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
