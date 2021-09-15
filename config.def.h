fd1992d993242215603c806c8f9a8729a6a595ec
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2021-09-15 11:40:11 +0700

    patch successfully barheight

--- config.def.h
+++ config.def.h
@@ -10,6 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 20;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "Ubuntu Mono:size=9", "monospace:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
